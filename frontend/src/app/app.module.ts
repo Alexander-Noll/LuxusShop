3f97849408c97a24b9bebdf37dbb7d807f43d695
Alex <alex_noll@icloud.com>
2023-01-31 03:13:03 +0100

    adding new components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LatestComponent } from './latest/latest.component';
+import { LoginComponent } from './login/login.component';
+import { AboutUsComponent } from './about-us/about-us.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LatestComponent,
+    LoginComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,

1e276f81e92c0b685d22b9cbec45417454792cdd
Alex <alex_noll@icloud.com>
2023-02-25 03:11:57 +0100

    log in page

--- frontend/src/app/components/pages/login/login.component.ts
+++ frontend/src/app/components/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import * as bcrypt from 'bcrypt';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,6 +11,21 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    }
+
+  username: string = '';
+  password: string = '';
+
+  onSubmit(): void {
+    // Generate a random salt value using bcrypt
+
+    const salt = bcrypt.genSaltSync(10);
+
+    // Hash the password with the salt using bcrypt
+    const hashedPassword = bcrypt.hashSync(this.password, salt);
+
+    // Save the username, hashed password, and salt in the database
+    // You can use a backend API to do this using HTTP requests or a library like Mongoose
+  }
 }
